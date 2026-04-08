27b6b787bee849beece2bb6b542104983da74132
etechpro <technov8ive@gmail.com>
2026-04-08 19:00:54 +0100

    update feature

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,6 +1,6 @@
 """FastAPI API for the URL shortener service.
 
-Exposes two endpoints:
+Exposes 2 endpoints:
 -  POST /shorten: accepts a long URL and returns a generated short code with the original URL.
 -  GET /{short_code}: Redirects the clients to the original long URL if the short code exists.
 """
