7c286bfb2235e2ec2862e03ed8d990ecb6d3ab75
etechpro <technov8ive@gmail.com>
2026-04-09 19:51:25 +0100

    add redis caching

--- app/core/cache.py
+++ app/core/cache.py
@@ -5,6 +5,11 @@ from redis.asyncio import Redis
 # Load environment variables from .env
 load_dotenv()
 
+REDIS_HOST=os.getenv("REDIS_HOST")
+REDIS_PORT=os.getenv("REDIS_PORT")
+REDIS_DB=os.getenv("REDIS_DB")
+REDIS_PASSWORD=os.getenv("REDIS_PASSWORD")
+
 async def init_redis():
     """Initialize Redis connection on app startup."""
     global redis
