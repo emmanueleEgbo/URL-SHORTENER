de78fc3ccc68e8ca3a8e537c4b68146cc6ce0ba2
etechpro <technov8ive@gmail.com>
2026-04-27 22:22:21 +0100

    feat: add webhook_schema.py for webhooks schema

--- app/schemas/webhooks_schema.py
+++ app/schemas/webhooks_schema.py
@@ -8,3 +8,9 @@ class WebhookEvent(str, Enum):
     URL_CREATED = "url.created"
     URL_CLICKED = "url.clicked"
     URL_DELETED = "url.deleted"
+
+
+class WebhookCreate(BaseModel):
+    name: str
+    url: str
+    event: List[WebhookEvent]
