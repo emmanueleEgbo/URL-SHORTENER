b1475188a8123d31f404b64618ad79f5e1c1a92f
etechpro <technov8ive@gmail.com>
2026-04-27 19:13:11 +0100

    feat: add webhook_schema.py for webhooks schema

--- app/schemas/webhooks_schema.py
+++ app/schemas/webhooks_schema.py
@@ -2,3 +2,9 @@ from datetime import datetime
 from enum import Enum
 from typing import List
 from pydantic import BaseModel
+
+
+class WebhookEvent(str, Enum):
+    URL_CREATED = "url.created"
+    URL_CLICKED = "url.clicked"
+    URL_DELETED = "url.deleted"
