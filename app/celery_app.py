157de3eb239c86d9e742c87f8f11deea4ccfc42c
etechpro <technov8ive@gmail.com>
2026-05-03 23:02:49 +0100

    feat: add celery

--- app/celery_app.py
+++ app/celery_app.py
@@ -4,4 +4,11 @@ from app.core.config import settings
 
 
 def create_celery_app() -> Celery:
-    pass
+    app = Celery(
+        "url_shortener",
+        broker=settings.celery_broker_url,
+        backend=settings.celery_backend_url,
+        include=[
+
+        ],
+    )
