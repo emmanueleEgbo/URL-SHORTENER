929047511022f756d5f663601f049d0172bddb71
etechpro <technov8ive@gmail.com>
2026-05-09 22:14:34 +0100

    bug fix

--- app/webhook_receiver_app.py
+++ app/webhook_receiver_app.py
@@ -5,7 +5,7 @@ from fastapi import FastAPI, Request
 
 app = FastAPI()
 
-@app.post("/webhook/receiver")
+@app.post("/webhook")
 async def receive_webhook(request: Request):
     """
     Receive incoming webhook events.
